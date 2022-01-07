d6b677a97e88e6dd6541a142994b1b1776a4bf75
epitaq <somaok2004@gmail.com>
2022-01-07 17:21:25 +0900

    最後のフレームを残す

--- main.py
+++ main.py
@@ -18,31 +18,30 @@ def create_pi (im, width):
     thresh = 128
     im_bool = (im_g > thresh) * 1
     #cv2.imwrite('test2.png',im_bool)
-    s = np.shape(im_bool)
-    #print(s[0])
     for i in im_bool:
         for i1 in i:
             print(pixel[i1],end='')
         print()
-    ow = "\033["+str(s[0]+1)+"A"
-    print(ow)
+
 
 def create_mv (path, width):
     cap = cv2.VideoCapture(path)
     sleep_time = (1000//(cap.get(cv2.CAP_PROP_FPS)))/1000
+    ret, frame = cap.read()
+    sz = np.shape(frame)
+    ow = '\033[' + str(sz[0]+1) + 'A'
     while True:
         ret, frame = cap.read()
         if ret:
+            print(ow)
             create_pi(frame, width)
             time.sleep(sleep_time)
         else:
             break
-    
-
 
 
 if __name__ == "__main__":
     width = 80
     # img = cv2.imread('test1.jpg')
     # create_pi(img, width)
-    create_mv('badapple.mp4', width)
+    create_mv('test.mp4', width)
