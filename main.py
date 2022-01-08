5b441fe35dfc6a9b31a5dedfa4bfd2dc20709a7f
epitaq <somaok2004@gmail.com>
2022-01-08 19:33:43 +0900

    速度遅いって言われたから実行速度を元に変更

--- main.py
+++ main.py
@@ -27,8 +27,10 @@ def create_pi (im, width):
 
 
 def create_mv (path, width):
+    #tim = []
     cap = cv2.VideoCapture(path)
-    sleep_time = (1000//(cap.get(cv2.CAP_PROP_FPS)))/1000
+    sleep_time_d = (10000//(cap.get(cv2.CAP_PROP_FPS)))/10000
+    sleep_time = sleep_time_d
     #上書き表示用の定数
     ret, frame = cap.read()
     im_s = scale_to_width(frame, width)
@@ -36,18 +38,26 @@ def create_mv (path, width):
     ow = '\033[' + str(sz[0]+1) + 'A'
     create_pi(frame, width)
     #2フレーム以降の表示
+    time0 = time.time()
     while True:
+        time0 = time.time()
         ret, frame = cap.read()
         if ret:
             print(ow)
             create_pi(frame, width)
+            time1 = time.time()
+            sleep_time = sleep_time_d - (time1 - time0)
+            if sleep_time <= 0:
+                sleep_time = 0.001
+            #tim.append(sleep_time)
             time.sleep(sleep_time)
         else:
             print()
             break
+    #print(tim)
 
 
 
 if __name__ == "__main__":
     width = 50
-    create_mv('test.mp4', width)
+    create_mv('badapple.mp4', width)
