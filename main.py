8b2dd521ee31528901ad64b3ff0d6e4dcfd530fd
epitaq <somaok2004@gmail.com>
2022-01-09 21:18:17 +0900

    :)

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def create_mv (path, width):
 
 
 if __name__ == "__main__":
-    width = 40
-    create_mv('ba540.mp4', width)
+    width = 80
+    create_mv('badapple.mp4', width)
     # im = cv2.imread('test1.jpg')
     # im1 = create_pi_gray(im, width)
