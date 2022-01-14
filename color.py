5da07d7fb8a01f2a9d38c866043489423dce6bcc
epitaq <somaok2004@gmail.com>
2022-01-14 21:55:19 +0900

    rgb化

--- color.py
+++ color.py
@@ -1,5 +1,12 @@
-for i in range(0,8):
-    print('\033[3{}m'.format(i) + '{}'.format(i) ,end=(' '))
+# for i in range(0,8):
+#     print('\033[3{}m'.format(i) + '{}'.format(i) ,end=(' '))
 
 for i in range(255):
     print('\033[38;5;{}m'.format(i) + '{}'.format(hex(i)) ,end=' ')
+else:
+    print()
+
+for r in range(1,255,50):
+    for g in range(1,255,50):
+        for b in range(1,255,50):
+            print('\033[38;2;{};{};{}m'.format(r,g,b,) + hex(int(str(r)+str(g)+str(b))) ,end='')
