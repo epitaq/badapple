2c3d77fd21a6588c2fafacb39583e32834e6de77
epitaq <somaok2004@gmail.com>
2022-01-14 20:48:35 +0900

    wslでtest

--- color.py
+++ color.py
@@ -1,2 +1,6 @@
 for i in range(0,8):
-    print('\033[3{}m'.format(i) + 'test' + '\033[0m')
+    print('\033[3{}m'.format(i) + '{}'.format(i) ,end=(' '))
+
+for i in range(255):
+    print('\033[38;5;{}m'.format(hex(i)) + '{}'.format(hex(i)) ,end=' ')
+    #print(hex(i),end='')
