e04c6621e79f73b24aff6c743795067fa24fd3e9
epitaq <somaok2004@gmail.com>
2022-01-14 20:51:53 +0900

    iketa

--- color.py
+++ color.py
@@ -2,5 +2,4 @@ for i in range(0,8):
     print('\033[3{}m'.format(i) + '{}'.format(i) ,end=(' '))
 
 for i in range(255):
-    print('\033[38;5;{}m'.format(hex(i)) + '{}'.format(hex(i)) ,end=' ')
-    #print(hex(i),end='')
+    print('\033[38;5;{}m'.format(i) + '{}'.format(hex(i)) ,end=' ')
