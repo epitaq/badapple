1be92a757d1c0f0f88a26a65af6bdc2dca35bc3f
epitaq <somaok2004@gmail.com>
2022-01-15 15:54:42 +0900

    :)

--- color.py
+++ color.py
@@ -13,4 +13,10 @@ for r in range(1,255,50):
             id = 16 + 36*r//h + 6*g//h + b//h
             print('\033[38;5;{}m'.format(id) + '{}'.format(id) + '\033[0m' ,end=' ')
             print('({},{},{})'.format(r//h,g//h,b//h) ,end = ' ,')
-            #print('\033[38;2;{};{};{}m'.format(r,g,b,) + hex(int(str(r)+str(g)+str(b))) )
+            print('\033[38;2;{};{};{}m'.format(r,g,b,) + hex(int(str(r)+str(g)+str(b))) )
+
+
+# 0-  7:  standard colors (as in ESC [ 30–37 m)
+# 8- 15:  high intensity colors (as in ESC [ 90–97 m)
+# 16-231:  6 × 6 × 6 cube (216 colors): 16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
+# 232-255:  grayscale from black to white in 24 steps
