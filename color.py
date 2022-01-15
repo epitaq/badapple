7ace42bd3873892b2d082c29d771f461877aa49a
epitaq <somaok2004@gmail.com>
2022-01-15 16:10:59 +0900

    test

--- color.py
+++ color.py
@@ -10,10 +10,10 @@ for r in range(1,255,50):
     for g in range(1,255,50):
         for b in range(1,255,50):
             h = 42.6
-            id = int(16 + round(36*r/h) + round(6*g/h) + round(b/h))
+            id = int(16 + 36*(r//h) + 6*(g//h) + (b//h))
             print('\033[38;5;{}m'.format(id) + '{}'.format(id) + '\033[0m' ,end=' ')
             print('({},{},{})'.format(r//h,g//h,b//h) ,end = ' ,')
-            print()
+            #print()
             print('\033[38;2;{};{};{}m'.format(r,g,b,) + hex(int(str(r)+str(g)+str(b))) )
 
 
