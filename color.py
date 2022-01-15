fee2ea19f3307770bef5d4b1db9f9628f7331935
epitaq <somaok2004@gmail.com>
2022-01-15 16:03:43 +0900

    tarinai

--- color.py
+++ color.py
@@ -10,10 +10,11 @@ for r in range(1,255,50):
     for g in range(1,255,50):
         for b in range(1,255,50):
             h = 51
-            id = 16 + 36*r//h + 6*g//h + b//h
+            id = int(16 + round(36*r/h) + round(6*g/h) + round(b/h))
             print('\033[38;5;{}m'.format(id) + '{}'.format(id) + '\033[0m' ,end=' ')
             print('({},{},{})'.format(r//h,g//h,b//h) ,end = ' ,')
-            print('\033[38;2;{};{};{}m'.format(r,g,b,) + hex(int(str(r)+str(g)+str(b))) )
+            print()
+            #print('\033[38;2;{};{};{}m'.format(r,g,b,) + hex(int(str(r)+str(g)+str(b))) )
 
 
 # 0-  7:  standard colors (as in ESC [ 30–37 m)
